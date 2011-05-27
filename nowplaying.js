2fda8134ba181e1e2cb630bcac29fd6b6aadb216
Johannes Raggam <raggam-nl@adm.at>
2011-05-27 14:24:26 +0200

    add nowplaying

---
+++ nowplaying.js
@@ -0,0 +1,15 @@
+jQuery(document).ready(jQuery.ajax({
+    url: '/get_current',
+    dataType: 'json',
+    success: function(data) {
+        var show = data.show;
+        var artist = data.artist;
+        var title = data.title;
+        var nop_str = show ? show : '';
+        nop_str += nop_str && (artist || title) ? ': ' : '';
+        nop_str += artist ? artist : '';
+        nop_str += artist && title ? ' | ' : '';
+        nop_str += title ? title : '';
+        jQuery("#onair input").val(nop_str);
+    }
+}));
