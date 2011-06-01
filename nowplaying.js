1ba28a31c22ec89bf6ea024f69f94cb69a922ead
Johannes Raggam <raggam-nl@adm.at>
2011-06-01 13:41:23 +0200

    bugfix 'is not a function'

--- nowplaying.js
+++ nowplaying.js
@@ -1,15 +1,20 @@
-jQuery(document).ready(jQuery.ajax({
-    url: '/get_current',
-    dataType: 'json',
-    success: function(data) {
-        var show = data.show;
-        var artist = data.artist;
-        var title = data.title;
-        var nop_str = show ? show : '';
-        nop_str += nop_str && (artist || title) ? ': ' : '';
-        nop_str += artist ? artist : '';
-        nop_str += artist && title ? ' | ' : '';
-        nop_str += title ? title : '';
-        jQuery("#onair input").val(nop_str);
-    }
-}));
+jQuery(document).ready(function() {
+    jQuery.ajax({
+        url: '/get_current',
+        dataType: 'json',
+        success: function(data) {
+            var show = data.show;
+            var artist = data.artist;
+            var title = data.title;
+            var nop_str = show ? show : '';
+            nop_str += nop_str && (artist || title) ? ': ' : '';
+            nop_str += artist ? artist : '';
+            nop_str += artist && title ? ' | ' : '';
+            nop_str += title ? title : '';
+            jQuery("#onair input").val(nop_str);
+        }});
+});
+/*jQuery("#onair input").focus(function(event) {
+    event.preventDefault();
+    window.location.href = jQuery("#onair a").attr("href");
+});*/
