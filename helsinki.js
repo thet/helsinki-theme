82732f0026e7a843dfe9b821cfe7559a87f2e5d0
Johannes Raggam <raggam-nl@adm.at>
2014-03-13 23:11:18 +0100

    simplify layout for mach-mit and support-us, randomize item in mach-mit

--- helsinki.js
+++ helsinki.js
@@ -1,8 +1,24 @@
-jQuery(document).ready(function() {
+// From: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
+// Returns a random integer between min and max
+// Using Math.round() will give you a non-uniform distribution!
+function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+jQuery(document).ready(function () {
+
+    (function () {
+        // portlet static randomizer
+        var min = 1;
+        var max = jQuery('.portlet-static-mach-mit dd a').length;
+        var show = getRandomInt(min, max);
+        jQuery('.portlet-static-mach-mit dd a:nth-child(' + show + ')').css('display', 'block');
+    }());
+
     jQuery.ajax({
         url: '/get_current',
         dataType: 'json',
-        success: function(data) {
+        success: function (data) {
             var show = data.show;
             var artist = data.artist;
             var title = data.title;
@@ -12,5 +28,6 @@ jQuery(document).ready(function() {
             nop_str += artist && title ? ' | ' : '';
             nop_str += title ? title : '';
             jQuery("#onair input").val(nop_str);
-        }});
+        }
+    });
 });
